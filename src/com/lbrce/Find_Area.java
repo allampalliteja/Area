5ac17f86e5402743e28f6ebee53922a18ec42912
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-10-19 12:28:32 +0530

    team member commited

---
+++ src/com/lbrce/Find_Area.java
@@ -0,0 +1,12 @@
+package com.lbrce;
+
+public class Find_Area {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 public static double Cal(double p,double r,double t) {
+	        	//complete the code
+	        }
+	}
+
+}
